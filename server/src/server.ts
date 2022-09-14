e965eb15fb3bb2ea674a6cce57a6960ff4338a37
JFmaia <jfmaia741@gmail.com>
2022-09-14 12:01:43 -0300

    feat: Criando listagem de anúncios por jogos.

--- server/src/server.ts
+++ server/src/server.ts
@@ -37,15 +37,33 @@ app.post('/ads', (request, response) => {
 })
 
 //listamento de anúncio por game
-app.get('/games/:id/ads', (request, response) => {
-    // const gameId = request.params.id;
+app.get('/games/:id/ads', async(request, response) => {
+    const gameId = request.params.id;
 
-    return response.json([
-        {id:1, name:'Anuncio 1'},
-        {id:2, name:'Anuncio 2'},
-        {id:3, name:'Anuncio 3'},
-        {id:4, name:'Anuncio 4'},
-    ])
+    const ads = await prisma.ad.findMany({
+        select:{
+            id:true,
+            name:true,
+            weekDays: true,
+            userVoiceChannel:true,
+            yearsPlaying:true,
+            hourStart: true,
+            hourEnd: true,
+        },
+        where:{
+            gameId,
+        },
+        orderBy:{
+            createAt:'desc'
+        }
+    })
+
+    return response.json(ads.map(ad => {
+        return {
+            ...ad,
+            weekDays: ad.weekDays.split(',')
+        }
+    }))
 })
 
 //buscar discord por id do anúncio
