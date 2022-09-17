fa3d3d1d80288ac07dfad99757657d7c2ddaa4c5
JFmaia <jfmaia741@gmail.com>
2022-09-17 13:11:51 -0300

    chore: Pegango jogos da api e listando.

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,12 +1,20 @@
+import { useEffect, useState} from 'react'
 import { View, Image, FlatList } from 'react-native';
 import { styles } from './styles';
-
-import {GAMES} from '../../utils/games';
 import logoImg from '../../assets/logo-nlw-esports.png'
 import { Heading } from '../../components/Heading';
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 
 export function Home() {
+
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() => {
+    fetch('http://192.168.0.111:3333/games')
+      .then(response => response.json())
+        .then(data => setGames(data))
+  },[]);
+
   return (
     <View style={styles.container}>
       <Image 
@@ -16,7 +24,7 @@ export function Home() {
       <Heading title={'Encontre seu duo!'} subtitle={'Selecione o game que deseja jogar...'}/>
 
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={item => item.id}
         renderItem={({item})=>(
           <GameCard data={item}/>
