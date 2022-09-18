5f47c999f0729e607e125aeb58e6119b8d0739a0
JFmaia <jfmaia741@gmail.com>
2022-09-17 21:05:33 -0300

    chore: Adicionando novo componentes em tela.

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,18 +1,91 @@
+import { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import {useRoute, useNavigation} from '@react-navigation/native';
+import {Entypo} from '@expo/vector-icons';
+
+import logoImg from '../../assets/logo-nlw-esports.png';
+
+import { DuoCard, DuoCardProps } from '../../components/DuoCard';
 import { Background } from '../../components/Background';
-import {useRoute} from '@react-navigation/native';
 import { styles } from './styles';
 import { GameParams } from '../../@types/navigation';
 
+import { TouchableOpacity, View, Image, FlatList, Text } from 'react-native';
+
+import { THEME } from '../../theme';
+import { Heading } from '../../components/Heading';
+
+
 export function Game() {
 
+  const [duo, setDuos] = useState<DuoCardProps[]>([]);
+
+  const navigation = useNavigation();
   const route = useRoute();
   const game = route.params as GameParams;
+
+ 
+
+  function handleGoBack(){
+    navigation.goBack();
+  }
+
+  useEffect(() => {
+    fetch(`http://192.168.0.111:3333/games/${game.id}/ads`)
+      .then(response => response.json())
+        .then(data => {
+          setDuos(data);
+        })
+  },[]);
   
   return (
     <Background>
       <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={handleGoBack}>
+            <Entypo
+              name='chevron-thin-left'
+              color={THEME.COLORS.CAPTION_300}
+              size={20}
+            />
+          </TouchableOpacity>
+          <Image
+            source={logoImg}
+            style={styles.logo}
+          />
 
+          <View style={styles.right}/>
+        </View>
+
+        <Image
+          source={{uri: game.bannerUrl}}
+          style= {styles.cover}
+          resizeMode="cover"
+        />
+
+        <Heading 
+            title={game.title} 
+            subtitle={'Conecte-se e comece a jogar!'}
+        />
+        <FlatList
+          data={duo}
+          keyExtractor={item => item.id}
+          horizontal
+          style={styles.containerList}
+          contentContainerStyle={[duo.length >0 ? styles.contentList : styles.emptyListContent]}
+          showsHorizontalScrollIndicator = {false}
+          renderItem={({item}) => (
+            <DuoCard 
+              data={item}
+              onConnect={()=>{}}
+            />
+          )}
+          ListEmptyComponent={() => (
+            <Text style={styles.emptyListText}>
+              Não há anúncios publicados ainda.
+            </Text>
+          )}
+        />
       </SafeAreaView >
     </Background>
   );
